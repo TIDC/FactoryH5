9dd22f3563a80dea78b4850f5f3eb2a762be12a3
zjlian <zjlianzjlianzjlian@outlook.com>
2018-03-20 11:50:06 +0800

    iview 测试

---
+++ src/components/iViewTest.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <p>表单</p>
+        <Input 
+            v-model="value" 
+            placeholder="Enter something..." 
+            style="width: 300px" /><br>
+        <Input 
+            v-model="value" 
+            placeholder="Enter something..." 
+            style="width: 300px"
+            clearable="true" /><br>
+        <Input 
+            v-model="value" 
+            type="password"
+            placeholder="Enter something..." 
+            style="width: 300px"
+            clearable="true" /><br>
+        <Input 
+            v-model="value" 
+            type="textarea"
+            placeholder="Enter something..." 
+            style="width: 300px"
+            clearable="true" /><br>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                value: ''
+            }
+        }
+    }
+</script>
